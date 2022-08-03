1110a9f5531b25cf6f076dfa1a027d9e72fce21f
Danielle Martin <ordinaryworld2014@gmail.com>
2022-08-03 18:37:31 -0500

    list and retrieve working

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -73,9 +73,9 @@ export const Register = () => {
                 <fieldset>
                     <label htmlFor="verifyPassword"> Verify Password </label>
                     <input ref={verifyPassword} type="password" name="verifyPassword" className="form-control" placeholder="Verify password" required />
-                </fieldset>
+                </fieldset> 
                 <fieldset>
-                    <label htmlFor="verifyPassword"> Verify Password </label>
+                    <label htmlFor="bio"> Bio</label>
                     <textarea ref={bio} name="bio" className="form-control" placeholder="Let other gamers know a little bit about you..." />
                 </fieldset>
                 <fieldset style={{
